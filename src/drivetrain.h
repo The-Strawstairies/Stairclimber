96dad90813915be36a86c7e1f6fea0379d21f427
David Tarazi <davidtarazi@me.com>
2019-11-18 15:02:55 -0500

    integrated drive code with imu code - not tested

--- src/drivetrain.h
+++ src/drivetrain.h
@@ -10,4 +10,82 @@ struct Speeds {
 	float rb;
 };
 
+void send_motor_cmd(int l, int r) {
+		// drives motors at desired speeds
+		// (int left[-255 to 255], int right[-255 to 255]) -> void
+		// handles negative values as reverse direction
+		l_motor.setSpeed(l);
+		r_motor.setSpeed(r);
+}
+
+void drive_all(int speed) {
+		// drive all wheels at the same speed
+		send_motor_cmd(speed, speed);
+}
+
+void turn_counterclockwise(int speed){
+		// turn counterclockwise in place
+		send_motor_cmd(speed/2, -speed/2);
+}
+
+void turn_clockwise(int speed){
+		// turn clockwise in place
+		send_motor_cmd(-speed/2, speed/2);
+}
+
+void serialReader() {
+	// read the serial buffer for new operations which are:
+	// 	   START code -> S
+    //     STOP code  -> E
+	// 	   VEL code   -> V50.0
+	// NOTE: parse float is blocking, we'll have to see how messy that is.
+	// AND: assumes that the end of a line has a line ending character
+	if (Serial.available() > 0) {
+		if (Serial.peek() == 'S') {
+				// start the program
+				Serial.read();
+				Serial.println("starting the drive loop");
+				run = 1;
+		} else if (Serial.peek() == 'E') {
+				// end the program
+				Serial.read();
+				Serial.println("ending the drive loop");
+				mode = 0;
+				run = 0;
+		} else if (Serial.peek() == 'V') {
+				// read new derivative value
+				Serial.read();
+				float vel = Serial.parseFloat();
+				Serial.println("setting speed constant");
+				motor_speed.linvel = vel;
+		// Use modes for keyboard drive
+		} else if (Serial.peek() == 'R') {
+				// Stop
+				Serial.read();
+				mode = 0;
+		} else if (Serial.peek() == 'T') {
+				// Forwards
+				Serial.read();
+				mode = 1;
+		} else if (Serial.peek() == 'F') {
+				// Counter-clockwise turn in place
+				Serial.read();
+				mode = 2;
+		} else if (Serial.peek() == 'G') {
+				// Backwards
+				Serial.read();
+				mode = 3;
+		} else if (Serial.peek() == 'H') {
+				// Clockwise turn in place
+				Serial.read();
+				mode = 4;
+		} else {
+			while(Serial.available()) {
+				Serial.read();
+				delay(10);
+			}
+		}
+	}
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "Arduino.h"
 #include "Wire.h"
-#include "pid.h"
 #include "main.h"
 #include "Servo.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#include "pid.h"
+#include "drivetrain.h"
+#include "CytronMotorDriver.h"
 
 // object representing the gyro
 MPU6050 mpu;
@@ -13,13 +15,27 @@ MPU6050 mpu;
 Servo balancer;
 
 // ================================================================
+// ===               MOTOR / DRIVE SETUP                        ===
+// ================================================================
+// motors
+CytronMD l_motor(PWM_DIR, 3, 4); // PWM = Pin 3, DIR = Pin 4
+CytronMD r_motor(PWM_DIR, 3, 4); // PWM = Pin 3, DIR = Pin 4
+
+// start/stop variable
+int run = 0;
+
+// Use mode to shift between different drive styles
+int mode = 0;
+
+// struct for handling a pair of motor speeds
+struct Speeds motor_speed;
+
+// ================================================================
 // ===               GYRO SETUP                                 ===
 // ================================================================
 
 
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
+#define INTERRUPT_PIN 2  // use pin 2 for interupts
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -62,27 +78,34 @@ pid pid_control = pid(0, 1.0, 0.0, 0.0);
 unsigned long lastMillis;
 unsigned long wait = 1000; // ms to delay code block
 
+
+
 void setup(){
 	// Servo setup
 	balancer.attach(10);
-	balancer.write(12);
+	balancer.write(12); //initial angle
+
+	// Motor starting position
+	send_motor_cmd(0, 0);
+
 	// IMU config
 	Wire.begin();
 	Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
 
 	Serial.begin(115200);
 
+	// Initialize the imu
 	mpu.initialize();
 	pinMode(INTERRUPT_PIN, INPUT);
-
 	devStatus = mpu.dmpInitialize();
 
+	// set offsets for factory error
 	mpu.setXGyroOffset(220);
 	mpu.setYGyroOffset(76);
 	mpu.setZGyroOffset(-85);
-	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+	mpu.setZAccelOffset(1788);
 
-	// make sure it worked (returns 0 if so)
+	// make sure imu setup worked
 	if (devStatus == 0) {
 			// Calibration Time: generate offsets and calibrate our MPU6050
 			mpu.CalibrateAccel(6);
@@ -114,30 +137,68 @@ void setup(){
 			Serial.print(devStatus);
 			Serial.println(F(")"));
 	}
+
+
 }
 
 void loop(){
-		// if programming failed, don't try to do anything
-    // if (!dmpReady) return;
+	// drive control
+	// respond to serial operations
+	serialReader();
+	if (run == 1) {
+		// For drive commands
+		// T = forward
+		// F = counterclockwise turn in place
+		// G = backward
+		// H = clockwise turn in place
+		// R = stop
 
-		// wait for MPU interrupt or extra packet(s) available
+		// S = run
+		// E = stop running
+
+		switch(mode){
+			case 0:
+				drive_all(0);
+				break;
+			case 1:
+				drive_all(motor_speed.linvel);
+				break;
+			case 2:
+				turn_counterclockwise(motor_speed.linvel);
+				break;
+			case 3:
+				drive_all(-motor_speed.linvel);
+				break;
+			case 4:
+				turn_clockwise(motor_speed.linvel);
+				break;
+			default:
+				drive_all(0);
+				break;
+		}
+
+		// log values
+		// LOG,time,left,right,sensor_left, sensor_right
+		//Serial.print("LOG,Motors,");
+		//Serial.println(String(motor_speed.linvel));
+
+	} else {
+		drive_all(0);
+	}
+
+	// balance
+	// wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
         if (mpuInterrupt && fifoCount < packetSize) {
 						// try to get out of the infinite loop
 						fifoCount = mpu.getFIFOCount();
         }
         // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
+       
+		
+        
     }
-
+	// INT error handling
 		// reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
@@ -155,7 +216,7 @@ void loop(){
 				//  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
         Serial.println(F("FIFO overflow!"));
 
-				// otherwise, check for DMP data ready interrupt (this should happen frequently)
+	// otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
 
 				// read a packet from FIFO
@@ -165,7 +226,7 @@ void loop(){
 						// (this lets us immediately read more without waiting for an interrupt)
 						fifoCount -= packetSize;
 				}
-
+				// get current angle and map it to servo angle
 				currentAngle = ypr[2] * 180/M_PI;
 
 				double servoOutput = map(currentAngle, 0, 30, 12,42);
